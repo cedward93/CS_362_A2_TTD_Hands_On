09b27ec474811111a7ec547ae5dd73eba1753739
cedward93 <cedward1993@gmail.com>
2025-05-12 19:26:35 -0700

    added "if __name__ = '__main__'" statement

--- tests.py
+++ tests.py
@@ -7,3 +7,7 @@ class TestCheckPwd (unittest.TestCase):
     def test_valid_min_length(self):
         """tests a valid password length"""
         self.assertTrue(check_pwd("12345678"))
+
+
+if __name__ == '__main__':
+    unittest.main()
