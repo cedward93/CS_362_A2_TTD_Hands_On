888f59e4d1799d377adf1075e27157a1ac8ab98f
cedward93 <cedward1993@gmail.com>
2025-05-12 21:29:47 -0700

    added test_short_length to test an out of bounds entry and test_valid_length to test an entry that is within bounds.

--- tests.py
+++ tests.py
@@ -8,6 +8,15 @@ class TestCheckPwd (unittest.TestCase):
         """tests takes a blank entry and should return false"""
         self.assertFalse(check_pwd(""))
 
+    def test_short_length(self):
+        """testing for a length less than the min"""
+        self.assertFalse(check_pwd("Test.1"))
+
+    def testing_valid_length(self):
+        """testing for a valid length"""
+        self.assertTrue(check_pwd("Test.length13"))
+
+
 
 
 
